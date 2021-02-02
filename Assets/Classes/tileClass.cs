322119fc5ad55f4413413f54b818d6ddbc909454
ryansteeters <60243364+ryansteeters@users.noreply.github.com>
2021-02-02 16:38:20 -0500

    Board Space Class
    Added Scrabble file to contain 'ScrabbleNamespace', created class boardSpace in file boardSpaceClass, renamed tile file to tileClass - tile class name remains the same

---
+++ Assets/Classes/tileClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScrabbleNamespace
+{
+    public class tile : MonoBehaviour
+    {
+        private int pointVal;
+        private string letter;
+
+        public tile(int points, string character)
+        {
+            pointVal = points;
+            letter = character;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
+
