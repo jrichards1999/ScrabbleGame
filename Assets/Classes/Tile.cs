97b74c4b090bfbeb327e7722b850cecd533a43da
ryansteeters <60243364+ryansteeters@users.noreply.github.com>
2021-02-16 17:34:51 -0500

    Fix Tile Values
    Fixed the values in Tile.cs to reflect actual tile values

--- Assets/Classes/Tile.cs
+++ Assets/Classes/Tile.cs
@@ -13,32 +13,32 @@ namespace ScrabbleNamespace
         {
             switch (c)
             {
-                case 'A': return 9;
-                case 'B': return 2;
-                case 'C': return 2;
-                case 'D': return 4;
-                case 'E': return 12;
-                case 'F': return 2;
-                case 'G': return 3;
-                case 'H': return 2;
-                case 'I': return 9;
-                case 'J': return 1;
-                case 'K': return 1;
-                case 'L': return 4;
-                case 'M': return 2;
-                case 'N': return 6;
-                case 'O': return 8;
-                case 'P': return 2;
-                case 'Q': return 1;
-                case 'R': return 6;
-                case 'S': return 4;
-                case 'T': return 6;
-                case 'U': return 4;
-                case 'V': return 2;
-                case 'W': return 2;
-                case 'X': return 1;
-                case 'Y': return 2;
-                case 'Z': return 1;
+                case 'A': return 1;
+                case 'B': return 3;
+                case 'C': return 3;
+                case 'D': return 2;
+                case 'E': return 1;
+                case 'F': return 4;
+                case 'G': return 2;
+                case 'H': return 4;
+                case 'I': return 1;
+                case 'J': return 8;
+                case 'K': return 5;
+                case 'L': return 1;
+                case 'M': return 3;
+                case 'N': return 1;
+                case 'O': return 1;
+                case 'P': return 3;
+                case 'Q': return 10;
+                case 'R': return 1;
+                case 'S': return 1;
+                case 'T': return 1;
+                case 'U': return 1;
+                case 'V': return 4;
+                case 'W': return 4;
+                case 'X': return 8;
+                case 'Y': return 4;
+                case 'Z': return 10;
                 default: return 0;
             }
         }
