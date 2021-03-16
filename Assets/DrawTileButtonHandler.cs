dd34fb8130c75bc49ff21d4ee3f9b68d5db0efdf
Jonathan Richards <jrichards050399@gmail.com>
2021-03-16 14:18:51 -0400

    Not sure what changed here

--- Assets/DrawTileButtonHandler.cs
+++ Assets/DrawTileButtonHandler.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class DrawTileButtonHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public void SpawnTile() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
