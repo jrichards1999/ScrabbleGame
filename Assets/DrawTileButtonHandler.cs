052e9b4eaa4be5fa3735d48ec098ae4b2831da96
Jonathan Richards <jrichards050399@gmail.com>
2021-03-23 13:18:15 -0400

    Added E prefab and moved prefabs to the resources folder.

--- Assets/DrawTileButtonHandler.cs
+++ Assets/DrawTileButtonHandler.cs
@@ -4,7 +4,20 @@ using UnityEngine;
 
 public class DrawTileButtonHandler : MonoBehaviour
 {
-    public void SpawnTile() {
+    public GameObject[] MyPrefabs;
 
+    public void Start() {
+        MyPrefabs = Resources.LoadAll<GameObject>("Prefabs");
+    }
+
+    public void Update() {
+        
+    }
+
+    public void ButtonClicked() {
+        //GameObject newObject = Resources.Load("Assets/Prefabs/A") as GameObject;
+        Vector2 position = new Vector2(0, 0);
+        var MyPrefab = MyPrefabs[1];
+        Instantiate(MyPrefab, position, Quaternion.identity);
     }
 }
