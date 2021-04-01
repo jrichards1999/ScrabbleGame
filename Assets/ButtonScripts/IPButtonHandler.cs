9f72fbc460c075fa21faebfb5d9832785f52144f
Jonathan Richards <jrichards050399@gmail.com>
2021-04-01 15:57:27 -0400

    Added public IP stuff in preparation for mirror code.
    Also moved button handler scripts to their own folder.

---
+++ Assets/ButtonScripts/IPButtonHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IPButtonHandler : MonoBehaviour
+{
+    public Text IPtxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick() {
+        string publicIPAddress = GetIPAddress();
+        IPtxt.text = publicIPAddress;
+    }
+
+    static string GetIPAddress() {
+        String address = "";
+        WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+        using (WebResponse response = request.GetResponse())
+        using (StreamReader stream = new StreamReader(response.GetResponseStream())) {
+            address = stream.ReadToEnd();
+        }
+
+        int first = address.IndexOf("Address: ") + 9;
+        int last = address.LastIndexOf("</body>");
+        address = address.Substring(first, last - first);
+
+        return address;
+    }
+}
